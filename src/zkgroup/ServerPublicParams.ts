ba76c723aa9275449531a2c0ad00b7da6117059f
ehsun <ehsun.ahmadi@gmail.com>
2024-07-31 17:59:50 +0330

    new version with fixed notarySignature var

--- src/zkgroup/ServerPublicParams.ts
+++ src/zkgroup/ServerPublicParams.ts
@@ -1,16 +1,22 @@
 import ReactNativeLibsignalClientModule from "../ReactNativeLibsignalClientModule";
-import NotarySignature from "./NotarySignature";
+import type NotarySignature from "./NotarySignature";
 
 export default class ServerPublicParams {
-  readonly serialized: Uint8Array;
+	readonly serialized: Uint8Array;
 
-  constructor(serialized: Uint8Array) {
-    this.serialized = serialized;
-  }
+	constructor(serialized: Uint8Array) {
+		this.serialized = serialized;
+	}
 
-  verifySignature(message: Uint8Array, notarySignature: NotarySignature): void {
-    if (!ReactNativeLibsignalClientModule.serverPublicParamsVerifySignature(this.serialized, message, notarySignature.contents)) {
-      throw new Error("ServerPublicParams.verifySignature failed");
-    }
-  }
+	verifySignature(message: Uint8Array, notarySignature: NotarySignature): void {
+		if (
+			!ReactNativeLibsignalClientModule.serverPublicParamsVerifySignature(
+				this.serialized,
+				message,
+				notarySignature.serialized,
+			)
+		) {
+			throw new Error("ServerPublicParams.verifySignature failed");
+		}
+	}
 }
