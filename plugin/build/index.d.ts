f76839bd2614cf3ed46696e1fe823532e1c8d973
ehsun <ehsun.ahmadi@gmail.com>
2025-04-28 14:51:58 +0330

    cleanup & deleting build so i can add Podfile with git -f

--- plugin/build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ConfigPlugin } from 'expo/config-plugins';
-declare const withConfig: ConfigPlugin;
-export default withConfig;
--- plugin/build/withCoreLibraryDesugaring.d.ts
+++
@@ -1,3 +0,0 @@
-import { type ConfigPlugin } from '@expo/config-plugins';
-declare const withCoreLibraryDesugaring: ConfigPlugin;
-export default withCoreLibraryDesugaring;
